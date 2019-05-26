c47d235b08e9c3b91916906d389ce1d3d011aca7
Mario Antolovic <marioantolovic0@gmail.com>
2019-05-26 19:58:38 +0200

    listViewDesign

--- app/src/main/java/mario/antolovic/coderswagger/Controller/MainActivity.kt
+++ app/src/main/java/mario/antolovic/coderswagger/Controller/MainActivity.kt
@@ -2,12 +2,24 @@ package mario.antolovic.coderswagger.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import mario.antolovic.coderswagger.Model.Category
 import mario.antolovic.coderswagger.R
+import mario.antolovic.coderswagger.Services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
